0bc8f50d872151e213fe4e8619d66c7d0b2bff5b
luisman1519 <creatiffpublicidad@gmail.com>
2018-07-19 21:20:54 -0300

    yeogurt

---
+++ src/_scripts/main.js
@@ -0,0 +1,13 @@
+// Main javascript entry point
+// Should handle bootstrapping/starting application
+
+'use strict';
+
+import $ from 'jquery';
+import Link from '../_modules/link/link';
+
+$(() => {
+  new Link(); // Activate Link modules logic
+  console.log('Welcome to Yeogurt!');
+  console.log("Hola a todos");
+});
